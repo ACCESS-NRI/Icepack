0a4650f63aafeb4aa16739bc78d1f5fe58403781
Kieran Ricardo <kieran.ricardo@anu.edu.au>
2024-09-17 12:11:48 +1000

    scale ice fluxes by changing ice area

--- columnphysics/icepack_flux.F90
+++ columnphysics/icepack_flux.F90
@@ -299,7 +299,7 @@
 
       character(len=*),parameter :: subname='(set_sfcflux)'
 
-      raicen        = c1 !/ aicen
+      raicen        = c1 / aicen
 
 #ifdef CICE_IN_NEMO
 !----------------------------------------------------------------------
